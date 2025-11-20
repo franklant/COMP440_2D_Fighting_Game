4e4a93ceddd2120e724eb04879f86cf60102e960
franklant <frankboss349@gmail.com>
2025-11-20 10:28:42 -0500

    updating Character Slot positioning

--- Assets/Scripts/CharacterSlotContainterScript.cs
+++ Assets/Scripts/CharacterSlotContainterScript.cs
@@ -1,5 +1,12 @@
 using Mono.Cecil;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+// TODO: Save the coordinates of the slots original position so that we can refer to it when adding hover effects.
+// TODO: Fix padding so the slots succesfully align on the horizontal axis of the scree, ordered starting from the center of the 
+//       screen to the ends of both sides. i.e. [ - - - - - | - - - - - ]
+//       Where ' | ' is the center slot and ' - ' is the corresponding slots on either end.
 
 /// <summary>
 /// CharacterSlotContainerScript manages each of the character slots within the 'Character Slot Container' object.
@@ -10,6 +17,9 @@ public class CharacterSlotContainterScript : MonoBehaviour
     [Header("Container Attributes")]
     [Tooltip("The current number of slots within the container.")]
     public int currentNumberOfSlots = 0;
+
+    [Tooltip("The amount of padding in between each slot.")]
+    public int padding = 1;            // pixel padding
     private GameObject[] characterSlots;     // used to get the game object of the character slot
 
 
@@ -28,8 +38,52 @@ public class CharacterSlotContainterScript : MonoBehaviour
                 break;
             }
 
-            slotScript.ID = i;
+            slotScript.SetID(i);
             currentNumberOfSlots += 1;
+        }
+    }
+
+    /// <summary>
+    /// Updates the position of each slot relative to the number of slots in the container and the size of each slot.
+    /// </summary>
+    void UpdateSlotPositions()
+    {
+        int slotIndex = 0;          // keep track of the current slot index
+
+        foreach (GameObject characterSlot in characterSlots)
+        {
+            Vector3 scale = Vector3.zero;
+            CharacterSlotScript slotScript = characterSlot.GetComponent<CharacterSlotScript>();
+            if (slotScript == null)
+            {
+                Debug.LogError("Cannot update " + characterSlot.name + "'s Position");
+                break;
+            }
+
+            GameObject border = slotScript.GetBorder();
+
+            // Debug.Log("Instance Name: <color=yellow>" + characterSlot.name + "</color>.");
+            // Debug.Log("Border Name: <color=yellow>" + border.transform.parent.gameObject.name + "</color>.");
+
+            if (characterSlot.name.Equals(border.transform.parent.gameObject.name))
+            {
+                // <color=red>Error: </color>
+                Debug.Log("<color=green>Succesfully caught our border component!</color>");
+            } else
+            {
+                Debug.Log("<color=red>Unable to verify border component!</color>");
+            }
+
+            scale = border.transform.localScale;
+            
+            float relativeX = (-currentNumberOfSlots * scale.x * padding) / 2;      // calculate the relative postition based on the amout of cards  
+
+            float offsetX = slotIndex * scale.x + (slotIndex * padding);
+            Vector3 finalPosition = new Vector3(offsetX + relativeX, 0, 0);
+            slotScript.SetPosition(finalPosition);
+
+            // update the slot index
+            slotIndex += 1; 
         }
     }
 
@@ -41,7 +95,7 @@ public class CharacterSlotContainterScript : MonoBehaviour
             Debug.LogWarning("There are no cards in the container!");
 
         UpdateSlotIDs();
-        // Debug.Log(characterSlots.Length);
+        UpdateSlotPositions();
 
         if (currentNumberOfSlots == 0)
             Debug.LogWarning("Number of slots is currently ZERO!");
--- Assets/Scripts/CharacterSlotScript.cs
+++ Assets/Scripts/CharacterSlotScript.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
@@ -17,6 +18,73 @@ public class CharacterSlotScript : MonoBehaviour
     private GameObject characterSlotContainer;
     private CharacterSlotContainterScript containerScript;
 
+
+    /// <summary>
+    /// Used by the 'CharacterSlotContainerScript' to get the border of this instance.
+    /// The border is used to calculate the position and internal padding of each slot.
+    /// </summary>
+    /// <returns>A game object for the border of this instance.</returns>
+    public GameObject GetBorder()
+    {
+        GameObject border = transform.GetChild(0).gameObject;          // the border component should be the first component
+        //Debug.Log("Border Name: <color=yellow>" + border.name + "</color>");
+
+        if (border == null)
+            Debug.LogError("Could not find the border!");
+        
+        return border;
+    }
+
+    /// <summary>
+    /// Used to get the background of this instance.
+    /// </summary>
+    /// <returns>A game object for the background of this instance.</returns>
+    public GameObject GetBackground()
+    {
+        GameObject background = transform.GetChild(1).gameObject;          // the border component should be the first component
+        //Debug.Log("Border Name: <color=yellow>" + border.name + "</color>");
+
+        if (background == null)
+            Debug.LogError("Could not find the background!");
+        
+        return background;
+    }
+
+    /// <summary>
+    /// Used by the 'CharacterSlotContainerScript' to set the ID of this instance. 
+    /// </summary>
+    /// <param name="sID">The ID value used to set the ID of this instance.</param>
+    public void SetID(int sID)
+    {
+        ID = sID;
+    }
+
+    /// <summary>
+    /// Used by the 'CharacterSlotContainerScript' to set the position of this instance.
+    /// </summary>
+    /// <param name="sPosition">The position value used to set the position this instance.</param>
+    public void SetPosition(Vector3 sPosition)
+    {
+        transform.position = sPosition;
+        position = transform.position;      // debug
+    }
+
+    /// <summary>
+    /// - PROTOTYPE - Sets the background color for this instance.
+    /// </summary>
+    /// <param name="color">The 'Color' object used to set the background color of this instance.</param>
+    public void SetBackgroundColor(Color color)
+    {
+        GameObject background = GetBackground();
+        SpriteRenderer backgroundRenderer = background.GetComponent<SpriteRenderer>();
+
+        if (backgroundRenderer == null)
+            Debug.LogError("Could not find the Background object Sprite Renderer!");
+
+        // Set the color
+        backgroundRenderer.color = color;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +95,8 @@ public class CharacterSlotScript : MonoBehaviour
         containerScript = characterSlotContainer.GetComponent<CharacterSlotContainterScript>();
         if (containerScript == null)
             Debug.LogError(ID.ToString() + " Cannot find Character Container Script!");
+
+        // Debug.Log("Set Position: " + position);
     }
 
     // Update is called once per frame
