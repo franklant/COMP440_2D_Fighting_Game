1c92826f27c6dd4fa32b58b5761d8c7562bc0d4e
emmanuellemi03 <emmanuellemi03@gmail.com>
2025-12-03 08:23:54 -0500

    Added game audio

--- Assets/Video_CutScenes/IntroVideo.cs
+++ Assets/Video_CutScenes/IntroVideo.cs
@@ -4,23 +4,39 @@ using UnityEngine.Video;
 public class VideoEndDeactivate : MonoBehaviour
 {
     [Header("Video Settings")]
-    public VideoPlayer videoPlayer; // Assign your VideoPlayer component here
+    public VideoPlayer videoPlayer;
+
+    [Header("Audio Settings")]
+    public AudioSource mainMenuMusic; // Assign the separate music AudioSource here
 
     void Start()
     {
-        // If not assigned, automatically find the VideoPlayer on this GameObject
+        // 1. Setup Video
         if (videoPlayer == null)
             videoPlayer = GetComponent<VideoPlayer>();
 
-        // Subscribe to the event that triggers when the video finishes
         if (videoPlayer != null)
             videoPlayer.loopPointReached += OnVideoEnd;
+
+        // 2. Ensure music isn't playing while video is running
+        if (mainMenuMusic != null)
+        {
+            mainMenuMusic.Stop(); // Ensures silence at the start
+        }
     }
 
-    // Called automatically when the video finishes playing
     void OnVideoEnd(VideoPlayer vp)
     {
-        Debug.Log("Video finished! Deactivating object...");
+        Debug.Log("Video finished! Switching to Music...");
+
+        // 1. Start the music
+        if (mainMenuMusic != null)
+        {
+            mainMenuMusic.Play();
+        }
+
+        // 2. Deactivate the video object
+        // (Since the music is on a different object, it keeps playing)
         gameObject.SetActive(false);
     }
 }
