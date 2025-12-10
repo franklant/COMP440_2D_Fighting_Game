c0badf32ff774f994fc23a4c4b4091b28e724ac9
Sean-Crosdale <slcrosdale@aggies.ncat.edu>
2025-12-10 16:42:32 -0500

    Updated scripts

--- Assets/Scripts/FighterInput.cs
+++ Assets/Scripts/FighterInput.cs
@@ -2,84 +2,122 @@ using UnityEngine;
 
 public class FighterInput : MonoBehaviour
 {
+    [Header("Settings")]
+    public bool isPlayer1 = true;
+    public bool isGojo = false;
+    public bool isSukuna = false;
+    public bool isNaruto = false;
+    public bool isMadara = false;
     public Animator animator;
     //public Rigidbody2D myRigidBody;
     // Assign in Inspector or use default keys
     
-    private KeyCode lowPunchKey = KeyCode.J;
-    private KeyCode mediumPunchKey = KeyCode.K;
-    private KeyCode highPunchKey = KeyCode.L;
-
-    
-    private KeyCode lowKickKey = KeyCode.I;
-    private KeyCode mediumKickKey = KeyCode.O;
-    private KeyCode highKickKey = KeyCode.P;
 
 
     void Update()
     {
-        HandlePunchInputs();
-        HandleKickInputs();
+        HandleBasicInputs();
+        HandleSpecialInputs();
     }
 
-    void HandlePunchInputs()
+    void HandleBasicInputs()
     {
-        if (Input.GetKeyDown(lowPunchKey))
-            LowPunch();
+        if(isPlayer1){
+            if (Input.GetKeyDown(KeyCode.J))
+                Punch();
 
-        if (Input.GetKeyDown(mediumPunchKey))
-            MediumPunch();
-
-        if (Input.GetKeyDown(highPunchKey))
-            HighPunch();
-    }
-
-    void HandleKickInputs()
-    {
-        if (Input.GetKeyDown(lowKickKey))
-            LowKick();
-
-        if (Input.GetKeyDown(mediumKickKey))
-            MediumKick();
-
-        if (Input.GetKeyDown(highKickKey))
-            HighKick();
-    }
-
-    void LowPunch()   { 
-        Debug.Log("LP");
-        AudioManager.Instance.PlayWhoosh();
+            if (Input.GetKeyDown(KeyCode.K))
+                Kick();
         }
-    void MediumPunch(){ 
-        Debug.Log("MP");
+        else{
+            if (Input.GetKeyDown(KeyCode.G))
+                Punch();
+
+            if (Input.GetKeyDown(KeyCode.H))
+                Kick();
+        }
+    }
+
+    void HandleSpecialInputs()
+    {
+        if (isPlayer1)
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+                Special1();
+                
+
+            if (Input.GetKeyDown(KeyCode.O))
+                Special2();
+                
+
+            if (Input.GetKeyDown(KeyCode.P))
+                Special3();
+                
+        }
+        else{
+            if (Input.GetKeyDown(KeyCode.R))
+                Special1();
+                
+
+            if (Input.GetKeyDown(KeyCode.T))
+                Special2();
+                
+
+            if (Input.GetKeyDown(KeyCode.Y))
+                Special3();
+               
+        }
+    }
+
+    void Punch()   { 
+        Debug.Log("P");
+        animator.SetTrigger("Attack");
+        AudioManager.Instance.PlayPunch();
+        }
+    void Kick(){ 
+        Debug.Log("K");
+        animator.SetTrigger("Kick");
         AudioManager.Instance.PlayKick();
     }
-    void HighPunch()  { 
-        Debug.Log("HP");
-        AudioManager.Instance.PlayHit();
-    }
 
-    void LowKick()    { 
-        Debug.Log("LK");
+    void Special1()    { 
+        Debug.Log("Sp1");
+        animator.SetTrigger("Meter1");
         AudioManager.Instance.PlaySpecial();
-        AudioManager.Instance.VoiceLines();
+        VoiceLines();
     }
-    void MediumKick() { 
-        Debug.Log("MK");
+    void Special2() { 
+        Debug.Log("Sp2");
+        animator.SetTrigger("Meter2");
         AudioManager.Instance.PlaySpecial();
-        AudioManager.Instance.VoiceLines();
+        VoiceLines();
     }
-    void HighKick() { 
-        Debug.Log("HK");
+    void Special3() { 
+        Debug.Log("Sp3");
+        animator.SetTrigger("Meter3");
         AudioManager.Instance.PlaySpecial();
-        AudioManager.Instance.VoiceLines();
+        VoiceLines();
     }
 
+    void VoiceLines()
+    {
+        if (isGojo)
+        {
+            AudioManager.Instance.GojoVL();
+        }
+        else if (isNaruto)
+        {
+            AudioManager.Instance.NarutoVL();
+        }
+        else if (isSukuna)
+        {
+            AudioManager.Instance.SukunaVL();
+        }
 
-
-
-
-
-    
+        else if (isMadara)
+        {
+            AudioManager.Instance.MadaraVL();
+        }
+    }
 }
 
